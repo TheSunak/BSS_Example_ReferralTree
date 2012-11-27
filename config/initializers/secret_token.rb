9be437dcb743c0173ef0c3ecde184526ab82bd0c
Vishal Sunak <vishalsunak@gmail.com>
2012-11-27 13:21:56 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReferralTree::Application.config.secret_token = '5116b154766a17a5ae6fc36f959018f53ca5e3809f9a42b6f1968f9fc0fe21fb2132039be46a39c6e11762f1c535e5382a2b80952a7951e5121960a9f8bacac0'
